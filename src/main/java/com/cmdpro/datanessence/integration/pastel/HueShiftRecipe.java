db4fd73f48f3b18c785723d59e63a122f733686d
Gronglegrowth <fyredragonmagus@gmail.com>
2026-03-08 16:51:27 -0400

    no way he actually did it

--- src/main/java/com/cmdpro/datanessence/integration/pastel/HueShiftRecipe.java
+++ src/main/java/com/cmdpro/datanessence/integration/pastel/HueShiftRecipe.java
@@ -1,10 +1,9 @@
 package com.cmdpro.datanessence.integration.pastel;
 
 import com.cmdpro.datanessence.recipe.DataNEssenceRecipe;
-import com.cmdpro.datanessence.recipe.IHasEssenceCost;
 import net.minecraft.resources.ResourceLocation;
 
-public class HueShiftRecipe implements IHasEssenceCost, DataNEssenceRecipe {
+public class HueShiftRecipe implements DataNEssenceRecipe {
 
     @Override
     public ResourceLocation getMachineEntry() {
