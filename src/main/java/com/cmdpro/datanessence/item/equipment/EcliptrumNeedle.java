1953f25db5ef777edc179f582e4e38fdf6df7533
Gronglegrowth <fyredragonmagus@gmail.com>
2026-03-26 11:55:59 -0400

    needle stubs

---
+++ src/main/java/com/cmdpro/datanessence/item/equipment/EcliptrumNeedle.java
@@ -0,0 +1,13 @@
+package com.cmdpro.datanessence.item.equipment;
+
+import net.minecraft.world.item.Item;
+
+public class EcliptrumNeedle extends Item {
+
+    // these are crafted in sets of 6, and double as very fast armor-piercing ranged weapons that will shatter with each attack,
+    // falling apart on impact. Ecliptrum Needles stack to 24.
+
+    public EcliptrumNeedle(Properties properties) {
+        super(properties);
+    }
+}
