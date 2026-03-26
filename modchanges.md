35b5811c9d3085c15cdc76dfa3998b608386634e
Gronglegrowth <fyredragonmagus@gmail.com>
2026-03-26 12:45:08 -0400

    skyrim nils voice can't just throw the big logs on the fire, got to have the little bits too

---
+++ modchanges.md
@@ -0,0 +1,9 @@
+### Additions
+- Industrial Infuser recipe for more efficient Charcoal production (Esét)
+
+### Changes
+- Nerfed the output of Pure Netherite (Pastel) in the Mineral Purification Chamber (Esét)
+- Traversite Road can be crafted out of Calcite as well as Diorite (Esét)
+- Updated zh_cn (ChuijkYahus)
+
+### Fixes
--- src/main/java/com/cmdpro/datanessence/datagen/recipe/DnERecipeProvider.java
+++ src/main/java/com/cmdpro/datanessence/datagen/recipe/DnERecipeProvider.java
@@ -9,6 +9,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -29,8 +30,32 @@ public class DnERecipeProvider extends RecipeProvider {
         // new InfusionRecipeBuilder(new ItemStack(ItemRegistry.LUNAR_ESSENCE_SHARD.get()), Ingredient.of(Items.AMETHYST_SHARD), DataNEssence.locate("basics/lunar_strikes"), -1, Map.of(DataNEssenceRegistries.ESSENCE_TYPE_REGISTRY.getKey(EssenceTypeRegistry.LUNAR_ESSENCE.get()), 100f)).unlockedBy("has_shard", has(Items.AMETHYST_SHARD)).save(output);
 
         // Industrial Infuser Recipes
-        new InfusionRecipeBuilder(new ItemStack(Items.NETHERITE_SCRAP, 2), Ingredient.of(Items.ANCIENT_DEBRIS), DataNEssence.locate("machinery/industrial_infuser_processing"), -1, Map.of(DataNEssenceRegistries.ESSENCE_TYPE_REGISTRY.getKey(EssenceTypeRegistry.ESSENCE.get()), 500f)).unlockedBy("has_debris", has(Items.ANCIENT_DEBRIS)).save(output);
-        new InfusionRecipeBuilder(new ItemStack(Items.GLOWSTONE_DUST, 2), Ingredient.of(Items.GLOW_BERRIES), DataNEssence.locate("machinery/industrial_infuser_processing"), -1, Map.of(DataNEssenceRegistries.ESSENCE_TYPE_REGISTRY.getKey(EssenceTypeRegistry.ESSENCE.get()), 100f)).unlockedBy("has_berry", has(Items.GLOW_BERRIES)).save(output);
-        new InfusionRecipeBuilder(new ItemStack(BlockRegistry.TRAVERSITE_ROAD.get().asItem()), Ingredient.of(Items.DIORITE), DataNEssence.locate("tools/traversite_road"), -1, Map.of(DataNEssenceRegistries.ESSENCE_TYPE_REGISTRY.getKey(EssenceTypeRegistry.ESSENCE.get()), 50f)).unlockedBy("has_diorite", has(Items.DIORITE)).save(output);
+        new InfusionRecipeBuilder(
+                new ItemStack(Items.NETHERITE_SCRAP, 2),
+                Ingredient.of(Items.ANCIENT_DEBRIS),
+                DataNEssence.locate("machinery/industrial_infuser_processing"), -1,
+                Map.of(DataNEssenceRegistries.ESSENCE_TYPE_REGISTRY.getKey(EssenceTypeRegistry.ESSENCE.get()), 500f)
+        ).unlockedBy("has_debris", has(Items.ANCIENT_DEBRIS)).save(output);
+
+        new InfusionRecipeBuilder(
+                new ItemStack(Items.GLOWSTONE_DUST, 2),
+                Ingredient.of(Items.GLOW_BERRIES),
+                DataNEssence.locate("machinery/industrial_infuser_processing"), -1,
+                Map.of(DataNEssenceRegistries.ESSENCE_TYPE_REGISTRY.getKey(EssenceTypeRegistry.ESSENCE.get()), 100f)
+        ).unlockedBy("has_berry", has(Items.GLOW_BERRIES)).save(output);
+
+        new InfusionRecipeBuilder(
+                new ItemStack(BlockRegistry.TRAVERSITE_ROAD.get().asItem()),
+                Ingredient.of( Items.DIORITE, Items.CALCITE ),
+                DataNEssence.locate("tools/traversite_road"), -1,
+                Map.of(DataNEssenceRegistries.ESSENCE_TYPE_REGISTRY.getKey(EssenceTypeRegistry.ESSENCE.get()), 50f)
+        ).unlockedBy("has_diorite", has(Items.DIORITE)).save(output);
+
+        new InfusionRecipeBuilder(
+                new ItemStack(Items.CHARCOAL, 2),
+                Ingredient.of(ItemTags.LOGS_THAT_BURN),
+                DataNEssence.locate("machinery/industrial_infuser_processing"), -1,
+                Map.of(DataNEssenceRegistries.ESSENCE_TYPE_REGISTRY.getKey(EssenceTypeRegistry.ESSENCE.get()), 80f)
+        ).unlockedBy("has_log", has(ItemTags.LOGS_THAT_BURN)).save(output);
     }
 }
