41ad74898fa31b076b30e21a096ae49dc02e7a12
Qther <qther@tuta.io>
2026-05-13 17:57:07 +0800

    fix up serialization

--- src/main/java/EsetKalenko/Halcyon/api/misc/BlockPosGraphSerialization.java
+++ src/main/java/EsetKalenko/Halcyon/api/misc/BlockPosGraphSerialization.java
@@ -11,21 +11,22 @@ import java.util.List;
 
 public class BlockPosGraphSerialization {
     public static class SerializationGraph {
+        public List<BlockPos> vertices;
+        public List<BlockPosEdge> edges;
+
         public static final Codec<SerializationGraph> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
                 BlockPos.CODEC.listOf().fieldOf("vertices").forGetter((graph) -> graph.vertices),
                 BlockPosEdge.CODEC.listOf().fieldOf("edges").forGetter((graph) -> graph.edges)
         ).apply(instance, SerializationGraph::new));
+
         public SerializationGraph(List<BlockPos> vertices, List<BlockPosEdge> edges) {
             this.vertices = vertices;
             this.edges = edges;
         }
-        public List<BlockPos> vertices;
-        public List<BlockPosEdge> edges;
+
         public static Graph<BlockPos, BlockPosEdge> toGraph(SerializationGraph graphSerialization) {
             Graph<BlockPos, BlockPosEdge> graph = Graph.newDirected();
-            for (BlockPos i : graphSerialization.vertices) {
-                graph.addVertex(i);
-            }
+            graph.addVertices(graphSerialization.vertices);
             for (BlockPosEdge i : graphSerialization.edges) {
                 graph.addEdge(i.source, i.target, i);
             }
@@ -33,7 +34,7 @@ public class BlockPosGraphSerialization {
         }
         public static SerializationGraph fromGraph(Graph<BlockPos, BlockPosEdge> graph) {
             List<BlockPos> vertices = new ArrayList<>(graph.vertices());
-            List<BlockPosEdge> edges = graph.edges().stream().toList();
+            List<BlockPosEdge> edges = new ArrayList<>(graph.edges());
             return new SerializationGraph(vertices, edges);
         }
     }
