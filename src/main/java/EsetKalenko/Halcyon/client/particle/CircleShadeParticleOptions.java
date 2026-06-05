e134cd62e9d14eff636762962b6fd0e76f9eb376
Gronglegrowth <fyredragonmagus@gmail.com>
2026-06-05 04:10:43 -0400

    that was it,

--- src/main/java/EsetKalenko/Halcyon/client/particle/CircleShadeParticleOptions.java
+++ src/main/java/EsetKalenko/Halcyon/client/particle/CircleShadeParticleOptions.java
@@ -2,11 +2,12 @@ package EsetKalenko.Halcyon.client.particle;
 
 import EsetKalenko.Halcyon.registry.ParticleRegistry;
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 
-public class CircleShadeParticleOptions extends ConfigurableParticleOptions {
+public class CircleShadeParticleOptions extends ConfigurableParticleOptions implements ParticleOptions {
     @Override
     public ParticleType<?> getType() {
         return ParticleRegistry.CIRCLE_SHADE.get();
